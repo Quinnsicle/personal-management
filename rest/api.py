a68c79d81b13f380cab6f066d41827a4464afe26
Quinn O'Connor <qtoconnor@gmail.com>
2022-07-21 18:31:18 -0500

    basic CRUD api

--- rest/api.py
+++ rest/api.py
@@ -1,10 +1,12 @@
 import functools
 from re import U
 import json
+from werkzeug.exceptions import abort
 from flask import (
     Blueprint,
     g,
-    jsonify
+    jsonify,
+    request,
 )
 
 from rest.db import get_db
@@ -17,14 +19,63 @@ def test_data():
     return jsonify({'name': 'alice',
                     'email': 'alice@outlook.com'})
 
-@bp.route('/put', methods=['PUT'])
+@bp.route('/get', methods=['GET'])
+def query_records():
+
+    db = get_db()
+    event = (
+        db.execute(
+            "SELECT name, start_date_time, end_date_time, category, tags FROM activity"
+        )
+    ).fetchone()
+
+    
+    if event is None:
+        abort(404, f"event doesn't exist.")
+
+    return jsonify({'name': event['name'],
+                    'start_date_time': event['start_date_time'],
+                    'end_date_time': event['end_date_time'],
+                    'category': event['category'],
+                    'tags': event['tags']})
+
+@bp.route('/put', methods=['GET', 'PUT'])
 def create_record():
     db = get_db()
     db.execute(
-        "INSERT INTO activity (name, start_date_time, end_date_time, category, tags)" " VALUES (work, 2022-07-21T07:00:00.000, 2022-07-21T16:00:00.000, work, ?)",
-        (title, body, g.user["id"]),
+        "INSERT INTO activity (name, start_date_time, end_date_time, category, tags)" 
+        " VALUES ('work', datetime('2022-07-21 07:00'), datetime('2022-07-21 16:00'), 'work', 'aoeu')",
+        ()
     )
     db.commit()
 
+    return "created record"
+
+@bp.route('/post', methods=['POST'])
+def update_record():
     record = json.loads(request.data)
-    return jsonify(record)
+
+    db = get_db()
+    db.execute(
+        "UPDATE table"
+        "SET name = ?,"
+        "WHERE id = ? ",
+        (record['name'], record['id'])
+    )
+    db.commit()
+
+    return "updated record"
+    
+@bp.route('/delete', methods=['DELETE'])
+def delte_record():
+    record = json.loads(request.data)
+
+    db = get_db()
+    db.execute(
+        "DELETE from activity"
+        "WHERE start_date_time = ? AND end_date_time = ?",
+        (record['start_date_time'], record['end_date_time'])
+    )
+    db.commit()
+
+    return "deleted record"
--- rest/schema.sql
+++ rest/schema.sql
@@ -3,8 +3,8 @@ DROP TABLE IF EXISTS activity;
 CREATE TABLE activity (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
-  start_date_time TIMESTAMP NOT NULL,
-  end_date_time TIMESTAMP NOT NULL,
+  start_date_time TEXT NOT NULL,
+  end_date_time TEXT NOT NULL,
   category TEXT,
   tags TEXT
 );
