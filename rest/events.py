bb1583f3316a34fbd39a61352010ac5e495de65f
Quinn O'Connor <qtoconnor@gmail.com>
2022-08-30 20:30:08 -0500

    handle csv format

--- rest/events.py
+++ rest/events.py
@@ -1,3 +1,5 @@
+import csv
+import io
 import functools
 from re import U
 import json
@@ -19,17 +21,39 @@ bp = Blueprint("events", __name__, url_prefix="/api")
 
 class Events(MethodView):
 
+    @staticmethod
+    def output_format(events: list, format: str = 'json'):
+        if format == 'csv':
+            csv_out = io.StringIO()
+
+            keys = events[0].keys()
+
+            dict_writer = csv.DictWriter(csv_out, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(events)
+
+
+            return csv_out.getvalue()
+        
+        if format == 'json':
+            return json.dumps(events)
+
+        return "sorry, this api doesn't support " + format
+
+
     def get(self):
         year = request.args.get('year')
         week = request.args.get('week')
+        format = request.args.get('format')
+        if format is None:
+            format = 'json'
         if year and week:
-            print(week)
-            return json.dumps(read_all_records_within_week(year, week))
+            return self.output_format(read_all_records_within_week(year, week), format)
         if year:
-            print(year)
-            return json.dumps(read_all_records_within_year(year))
+            return self.output_format(read_all_records_within_year(), format)
+
         events = read_all_records()
-        return json.dumps(events)
+        return self.output_format(read_all_records(), format)
 
     def post(self):
         # create a new event
@@ -42,6 +66,7 @@ class Events(MethodView):
     def put(self, event_id):
         # update a single event
         pass
+
 
 
 event_view = Events.as_view('events_api')
